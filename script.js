fac23e04d75d139a623265c0a38a27f2c0af1709
ashtinlatting <rlatting92@gwu.edu>
2019-07-18 11:00:26 -0400

    functionality of first slide complete

--- script.js
+++ script.js
@@ -1,18 +1,99 @@
 let slides = document.querySelectorAll('#slides .slide')
 let currentSlide = 0
 let submitButton = document.querySelector('.submit')
-var wrongAnswers = document.querySelectorAll('.wrong')
-let correctAnswers = document.querySelectorAll('.correct')
+// var wrongAnswers = document.querySelectorAll('.wrong')
+// let correctAnswers = document.querySelectorAll('.correct')
 let allHeader = document.querySelectorAll('allHeader')
-
+let slideHeader = document.querySelector('.slide-1-header')
+let slideFirstBody = document.querySelector('.body-main-1')
+let slideSecondBody = document.querySelector('.body-main-2')
+let slideThirdBody = document.querySelector('.body-main-3')
+let slideFourthBody = document.querySelector('.body-main-4')
+let slideAllBodies = document.querySelectorAll('.body-main')
 
 //all q/a will be stored in an array of arrays
 //will need a for loop to cycle through them all, setting all of the questions to the applicable header
 //and one anser to each body element
 
 let myQuestions = [
-	["What Cleveland native made his album debut with MOTM?", "a) Eminem", "b) Machine Gun Kelly", "c) John Legend", "d) Kid Cudi", "Kid Cudi!"],
-	["Which of Kanye's albums were released as apart of a friendly competition with 50 Cent?", "a) MBDTF", "b) Graduation", "c) Late Registration", "d) 808's", "Graduation!!"],
+	'Who is the lead singer of Nirvana?',
+	'What is the answer to question 2?',
+	'What is the answer to question 3?',
+	'What is the answer to question 4?',
+	'What is the answer to question 5?',
+	'What is the answer to question 6?',
+	'What is the answer to question 7?',
+	'What is the answer to question 8?',
+	'What is the answer to question 9?',
+	'What is the answer to question 10?'
+]
+
+let myAnswers = [
+	{
+		wrongOne: "Chester Bennington",
+		wrongTwo: "Anthony Kiedis",
+		correct: "Kurt Cobain",
+		wrongThree: "Chad Kroeger"
+	},
+	{
+		wrong: "Answer a",
+		correct: "Answer b",
+		wrong: "Answer c",
+		wrong: "Answer d"
+	},
+	{
+		wrong: "A",
+		wrong: "B",
+		correct: "C",
+		wrong: "D"
+	},
+	{
+		wrong: " A",
+		wrong: " B",
+		wrong: " C",
+		correct: " D"
+	},
+	{
+		correct: "A",
+		wrong: "B",
+		wrong: "C",
+		wrong: "D"
+	},
+	{
+		wrong: "A",
+		wrong: "B",
+		correct: "C",
+		wrong: "D"
+	},
+	{
+		wrong: "A",
+		correct: "B",
+		wrong: "C",
+		wrong: "D"
+	},
+	{
+		wrong: "A",
+		wrong: "B",
+		wrong: "C",
+		correct: "D"
+	},
+	{
+		wrong: "A",
+		wrong: "B",
+		correct: "C",
+		wrong: "D"
+	},
+	{
+		correct: "A",
+		wrong: "B",
+		wrong: "C",
+		wrong: "D"
+	},
+
+]
+
+let correctAnswers = [
+			"Kurt Cobain", "Answer b", "C", "D", "A", "C", "B", "D", "C", "A"
 	]
 
 
@@ -23,6 +104,12 @@ submitButton.addEventListener('click', function(evt) {
 	slides[currentSlide].className = 'slide'
 	currentSlide = (currentSlide+1)%slides.length
 	slides[currentSlide].className = 'slide showing'
+
+	slideHeader.innerHTML = myQuestions[0]
+	slideFirstBody.innerHTML = myAnswers[0].wrongOne
+	slideSecondBody.innerHTML = myAnswers[0].wrongTwo
+	slideThirdBody.innerHTML = myAnswers[0].correct
+	slideFourthBody.innerHTML = myAnswers[0].wrongThree
 })
 
 
@@ -38,27 +125,27 @@ submitButton.addEventListener('click', function(evt) {
 
 
 
-	for (let i=0; i < wrongAnswers.length; i++) {
-		// console.log('test1')
-		wrongAnswers[i].addEventListener('click', function(evt) {
-			evt.preventDefault()
-			// console.log('test')
-			if (evt.target.className === "wrong") {
-			console.log('wrong!')
+// 	for (let i=0; i < wrongAnswers.length; i++) {
+// 		// console.log('test1')
+// 		wrongAnswers[i].addEventListener('click', function(evt) {
+// 			evt.preventDefault()
+// 			// console.log('test')
+// 			if (evt.target.className === "wrong") {
+// 			console.log('wrong!')
 	
-			}
-		})
+// 			}
+// 		})
 
-}
+// }
 
-for (let i=0; i < correctAnswers.length; i++) {
-	correctAnswers[i].addEventListener('click', function(evt) {
-		evt.preventDefault()
-		if (evt.target.className === "correct") {
-		console.log('correct!')
-			allHeader.innerHTML = myQuestions[0][0]
-		}
-	})
+// for (let i=0; i < correctAnswers.length; i++) {
+// 	correctAnswers[i].addEventListener('click', function(evt) {
+// 		evt.preventDefault()
+// 		if (evt.target.className === "correct") {
+// 		console.log('correct!')
+// 			allHeader.innerHTML = myQuestions[0][0]
+// 		}
+// 	})
 
-}
+// }
 
